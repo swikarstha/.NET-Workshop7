055813e4eb1d4ba9f22a34efdf812e716f6c1728
swikarstha <swikarstha5844@gmail.com>
2025-12-11 14:00:50 +0600

    task 2

--- obj/Debug/net10.0-maccatalyst/maccatalyst-x64/MauiApp1.AssemblyInfo.cs
+++ obj/Debug/net10.0-maccatalyst/maccatalyst-x64/MauiApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MauiApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+83a781c5b8d40bd8afbc5f7d86f583af9b17b1f7")]
 [assembly: System.Reflection.AssemblyProductAttribute("MauiApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MauiApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
